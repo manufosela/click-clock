801c58ff363090bc8eed8e0440a6f0ace98704df
Manuel Juan Fosela Águila <manufosela@gmail.com>
2019-11-18 23:02:51 +0100

    improvments and refactor

--- click-clock.js
+++ click-clock.js
@@ -19,8 +19,7 @@ class ClickClock extends LitElement {
     return {
       numDigits: { type: Number, attribute: 'num-digits' },
       maxValue: { type: Number, attribute: 'max-value' },
-      minValue: { type: Number, attribute: 'min-value' },
-      dotSize: { type: String, attribyte: 'dot-size' }
+      dotSize: { type: String, attribute: 'dot-size' }
     };
   }
 
@@ -60,18 +59,18 @@ class ClickClock extends LitElement {
       .clock .digit .cell.on {
         opacity: 1;
       }
-      .dot { position:relative; background:#FF0; z-index:10; width:var(--dot-size); height:var(--dot-size); }
+      .dot { position:relative; background:#FF0; z-index:10; width:var(--dot-size); height:var(--dot-size); max-width:16px; max-height:16px; }
     `;
   }
 
   constructor() {
     super();
     this.numDigits = 3;
-    this.maxValue = 999;
-    this.minValue = 0;
+    this.maxValue = 10 * this.numDigits - 1;
     this.dotSize = '16px';
     this.now = new Date();
     this.digit = new Array(this.numDigits);
+    this.countDigit = this.numDigits + 1;
 
     this.display = {
       0: [
@@ -167,10 +166,16 @@ class ClickClock extends LitElement {
     };
   }
 
+  connectedCallback() {
+    super.connectedCallback();
+    this.countDigit = this.numDigits + 1;
+  }
+
   firstUpdated() {
+    this.countDigit = this.numDigits + 1;
     this.grad = new Date(this.now.getTime() + this.maxValue * 1000);
     for (let i = 0; i <= this.numDigits; i++) {
-      this.digit[i + 2] = this.shadowRoot.querySelector('#digit-' + (i + 2));
+      this.digit[i + 1] = this.shadowRoot.querySelector('#digit-' + (i + 1));
     }
     requestAnimationFrame(this.start.bind(this));
   }
@@ -192,142 +197,39 @@ class ClickClock extends LitElement {
     this.now = new Date();
     let diff = String(this.grad.getTime() - this.now.getTime());
     let len = diff.length;
+    if (diff > 0) {
+      for (let i = 1; i <= this.countDigit; i++) {
+        this._renderDigit(this.digit[i], Number(diff.charAt(len - (i + 2))));
+      }
+      requestAnimationFrame(this.start.bind(this));
+    } else {
+      document.dispatchEvent(new CustomEvent('click-clock-end', {detail: {seconds: this.maxValue}}));
+    }
+  }
 
-    this._renderDigit(this.digit[2], Number(diff.charAt(len - 3)));
-    this._renderDigit(this.digit[3], Number(diff.charAt(len - 4)));
-    this._renderDigit(this.digit[4], Number(diff.charAt(len - 5)));
-    this._renderDigit(this.digit[5], Number(diff.charAt(len - 6)));
-
-    requestAnimationFrame(this.start.bind(this));
+  get digitTemplate() {
+    let dotDecPos = (this.dotSize > 16) ? -16 : -parseInt(this.dotSize);
+    let cssClass = (this.countDigit === 1) ? '' : 'second';
+    let comma = (this.countDigit === 1) ? html`<div class="dot" style="top:${dotDecPos}px; left:${dotDecPos}px;">O</div>` : '';
+    let nDots = [ ...Array(28).keys() ];
+    return html`
+      <div id="digit-${this.countDigit--}" class="digit ${cssClass}">
+        ${nDots.map(item => html`<div class="cell"></div>`)}
+        ${comma}
+      </div>
+    `;
   }
 
   render() {
-    let dotDecTop = -(parseInt(this.dotSize) + 2);
-    let dotDecLeft = (parseInt(this.dotSize) * 5) + 3;
+    let nDigits = [ ...Array(this.numDigits + 1).keys() ];
     return html`
+      <style>
+        .clock {
+          --dot-size: ${this.dotSize}px;
+        }
+      </style>
       <section class="clock">
-      <div id="digit-5" class="digit second">
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-        </div>
-        <div id="digit-4" class="digit second">
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-        </div>
-        <div id="digit-3" class="digit second">
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="dot" style="top:${dotDecTop}px; left:${dotDecLeft}px;">O</div>
-        </div>
-        <div id="digit-2" class="digit">
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-          <div class="cell"></div>
-        </div>
-
+        ${nDigits.map(item => html`${this.digitTemplate}`)}
       </section>
       <!--<button>Start</button><button>Pause</button><button>Stop</button>-->
     `;
